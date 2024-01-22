b59a45e64e066072f517b0370c61354b223ba8fc
Sam Geier <sg7849@nyu.edu>
2024-01-22 17:57:36 -0500

    Created temp_here view in views.py

--- meteo/views.py
+++ meteo/views.py
@@ -1,3 +1,16 @@
+import geocoder
+import requests
+from datetime import datetime
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def temp_here(request):
+    endpoint = "https://api.open-meteo.com/v1/forecast"
+    location = geocoder.ip('me').latlng
+    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m"
+    now = datetime.now()
+    hour = now.hour
+    meteo_data = requests.get(api_request).json()
+    temp = meteo_data['hourly']['temperature_2m'][hour]
+    return HttpResponse(f"The temperature is {temp} degrees.")
