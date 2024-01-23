7b8617b03d88263b09b1cb69534fc319eef2b83b
Sam Geier <sg7849@nyu.edu>
2024-01-22 22:10:36 -0500

    Created index.html file for formatting the website. url.py file for getting the website url set up. Updated features of the views.py file.

--- meteo/views.py
+++ meteo/views.py
@@ -3,14 +3,18 @@ import requests
 from datetime import datetime
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+
 
 def temp_here(request):
     endpoint = "https://api.open-meteo.com/v1/forecast"
     location = geocoder.ip('me').latlng
-    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m"
+    api_request = (f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m&temperature_unit"
+                   f"=fahrenheit")
     now = datetime.now()
     hour = now.hour
     meteo_data = requests.get(api_request).json()
     temp = meteo_data['hourly']['temperature_2m'][hour]
-    return HttpResponse(f"The temperature is {temp} degrees.")
+    template = loader.get_template('index.html')
+    context = {'temp':temp}
+    return HttpResponse(template.render(context, request))
